7a06b77deb5c95e027854eaa81d0f57177c5f8cc
Austin Roos <roos.austin@me.com>
2013-12-09 00:42:12 -0600

    updated routes and layout

--- app.rb
+++ app.rb
@@ -4,7 +4,16 @@ require 'mongo'
 require 'mongo_mapper'
 require 'date'
 
+SITE_TITLE = "Austin C. Roos"
+SITE_PASSWORD = "a"
+MONGOLAB_URI = "mongodb://acr:soapy323@ds039507.mongolab.com:39507/acr-site"
 
+regex_match = /.*:\/\/(.*):(.*)@(.*):(.*)\//.match(MONGOLAB_URI)
+host = "ds039507.mongolab.com"
+port = "39507"
+db_name = "acr-site"
+user = "acr"
+pw = "soapy323"
 
 MongoMapper.connection = Mongo::Connection.new(host, port)
 MongoMapper.database = db_name
@@ -12,20 +21,45 @@ MongoMapper.database.authenticate(user, pw)
 
 class Project
   include MongoMapper::Document
-  key :order,		Integer
-  key :name,		String
+  key :order,				Integer
+  key :name,				String
   key :description,	String
-  key :content,		String
+  key :content,			Array
   key :created_at,	Time
-  key :pics,		Array 
+  key :pics,				Array 
 end
+
+class Job
+	include MongoMapper::Document
+	key :order,				Integer
+	key :name,				String
+	key :description,	Array
+	key :start_date,	String
+	key :end_date,		String
+end
+
 
 helpers do 
 	include Rack::Utils
 	alias_method :h, :escape_html
 end
 
-get '/' do
+def split_paragraphs(text)
+	items = Array.new
+	text.split('\r\n').each do |paragraph|
+		items.push paragraph
+	end
+	return items
+end
+
+['/', '/welcome'].each do |path|
+	get path do 
+		@title = 'Welcome'
+		erb :welcome, :layout => :layout_welcome
+	end
+end
+
+get '/index' do
 	@header = 'Austin C. Roos'
 	@title = 'Home'
 	@label = 'Home'
@@ -36,10 +70,17 @@ get '/about' do
 	@header = 'About Me'
 	@title = 'About Me'
 	@label = 'About'
-	erb :About
+	erb :about
 end
 
-get '/projects' do
+get '/experience' do
+	@header = 'Experience'
+	@title = 'Experience'
+	@label = 'Experience'
+	erb :experience
+end
+
+get '/projects/?' do
 	@projects = Project.sort(:created_at.desc)
 	@header = 'Projects'
 	@title = 'Projects'
@@ -70,15 +111,19 @@ get '/contact' do
 end
 
 get '/add-project' do
-	@title = "Admin only"
-	erb :add_project
+	if settings.development?
+		@title = "Admin only"
+		erb :add_project
+	else
+		redirect '/'
+	end
 end
 
 post '/add-project' do
 	@p = Project.new
 	@p.name        = params[:name]
 	@p.description = params[:description]
-	@p.content     = params[:content]
+	@p.content     = split_paragraphs(params[:content])
 	@p.order	   = params[:order].to_i
 	@p.created_at  = Time.now
 	if params[:password] == SITE_PASSWORD
@@ -86,33 +131,6 @@ post '/add-project' do
 		redirect '/projects'
 	else
 		redirect '/add-project'
-	end
-end
-
-get '/edit/project/:id' do
-	@project = Project.get params[:id]
-	@title = "Edit project #{params[:name]}"
-	if @project
-		erb :edit
-	else 
-		redirect '/'
-	end
-end
-
-put '/edit/project/:id' do
-	p = Project.get params[:id]
-	unless p
-		redirect '/'
-	end
-	p.name = params[:name]
-	p.description = params[:description]
-	p.content = params[:content]
-	p.created_at = Time.now
-	if params[:password] == SITE_PASSWORD
-		p.save
-		redirect '/projects'
-	else
-		redirect '/edit/:id'
 	end
 end
 
